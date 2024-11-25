d79ec86108dbe7842fe0d417971f2aa4823bc208
malripo <malripo741205@gmail.com>
2024-11-25 12:14:06 +0900

    allowed hosts

--- a_core/settings.py
+++ a_core/settings.py
@@ -39,9 +39,11 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tenantsdeploy.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+# CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://tenantsdeploy.up.railway.app' ]
 
 INTERNAL_IPS = (
     '127.0.0.1',
